dd66121bc687506a23df2e215db38c026a90a43f
Luiz Ricardo <luizrgfelipe@gmail.com>
2024-07-28 23:50:29 -0300

    🛠  REFACTOR: update install

--- install.sh
+++ install.sh
@@ -3,4 +3,6 @@ git clone https://github.com/luizrgf2/backend-wpp-project.git
 git clone https://github.com/luizrgf2/frontend-wpp-project.git
 git clone https://github.com/luizrgf2/wpp_sender_ms.git
 
+cd wpp_project/
+
 docker compose up -d
